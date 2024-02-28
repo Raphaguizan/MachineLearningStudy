dc0c046759a6e508da68d411e88b61908f85863c
RGuizan <raphaelguizan@gmail.com>
2024-02-28 18:46:34 -0300

    perceptron graph

--- Assets/Perceptron/Scripts/InitCube.cs
+++ Assets/Perceptron/Scripts/InitCube.cs
@@ -2,22 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InitCube : MonoBehaviour {
+namespace MLS.Perceptron
+{
+	public class InitCube : MonoBehaviour
+	{
 
-	public float s;
-	public float c;
+		public float s;
+		public float c;
 
-	// Use this for initialization
-	void Awake () {
-		s = Random.Range(0.1f,2.0f);
-		this.transform.localScale *= s;
-		c = Random.Range(0.0f, 1.0f);
+		// Use this for initialization
+		void Awake()
+		{
+			s = Random.Range(0.1f, 2.0f);
+			this.transform.localScale *= s;
+			c = Random.Range(0.0f, 1.0f);
 
-		this.GetComponent<Renderer>().material.color = new Color(0,0,c);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+			this.GetComponent<Renderer>().material.color = new Color(0, 0, c);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
 	}
 }
