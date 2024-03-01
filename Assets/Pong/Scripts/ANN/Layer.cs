0b7a69b0c3dd12ea9d1717495c5b046f8f6535cf
RGuizan <raphaelguizan@gmail.com>
2024-03-01 18:31:11 -0300

    Import race project (not include large files kart tga textures)

--- Assets/Pong/Scripts/ANN/Layer.cs
+++ Assets/Pong/Scripts/ANN/Layer.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MLS.Pong
-{
-	public class Layer
+public class Layer {
+
+	public int numNeurons;
+	public List<Neuron> neurons = new List<Neuron>();
+
+	public Layer(int nNeurons, int numNeuronInputs)
 	{
-
-		public int numNeurons;
-		public List<Neuron> neurons = new List<Neuron>();
-
-		public Layer(int nNeurons, int numNeuronInputs)
+		numNeurons = nNeurons;
+		for(int i = 0; i < nNeurons; i++)
 		{
-			numNeurons = nNeurons;
-			for (int i = 0; i < nNeurons; i++)
-			{
-				neurons.Add(new Neuron(numNeuronInputs));
-			}
+			neurons.Add(new Neuron(numNeuronInputs));
 		}
 	}
 }
--- Assets/Pong/Scripts/ANN/Neuron.cs
+++ Assets/Pong/Scripts/ANN/Neuron.cs
@@ -3,26 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MLS.Pong
-{
-	public class Neuron
+public class Neuron {
+
+	public int numInputs;
+	public double bias;
+	public double output;
+	public double errorGradient;
+	public List<double> weights = new List<double>();
+	public List<double> inputs = new List<double>();
+
+	public Neuron(int nInputs)
 	{
+		float weightRange = (float) 2.4/(float) nInputs;
+		bias = UnityEngine.Random.Range(-weightRange,weightRange);
+		numInputs = nInputs;
 
-		public int numInputs;
-		public double bias;
-		public double output;
-		public double errorGradient;
-		public List<double> weights = new List<double>();
-		public List<double> inputs = new List<double>();
-
-		public Neuron(int nInputs)
-		{
-			float weightRange = (float)2.4 / (float)nInputs;
-			bias = UnityEngine.Random.Range(-weightRange, weightRange);
-			numInputs = nInputs;
-
-			for (int i = 0; i < nInputs; i++)
-				weights.Add(UnityEngine.Random.Range(-weightRange, weightRange));
-		}
+		for(int i = 0; i < nInputs; i++)
+			weights.Add(UnityEngine.Random.Range(-weightRange,weightRange));
 	}
 }
